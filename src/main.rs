f114542218266cd18b82111cce84fed18ceb6592
lycopersicum-t <anastasijanovik06@gmail.com>
2026-02-07 12:43:53 +0300

    ready guessing_game

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("---GUESS THE NUMBER!!!---");
 
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+
+    loop {
     println!("Please input your guess: ");
 
     let mut guess = String::new();
@@ -10,6 +15,21 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+
+    match guess.cmp(&secret_num) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
+    }
     println!("You guessed {guess}");
+}
+    
 }
 
